8eb5dbf241f1d10a4f12453e0221f604894e567f
manish7922 <thakurms7983@gmail.com>
2023-05-27 05:14:46 -0700

    send me data

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require("express");
 var app = express();
-const http = require("http");
-const socketIO = require("socket.io");
-const server = http.createServer(app);
+const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 app.use(express.json());
 
 // app.use("/socket.io", express.static(__dirname + "/node_modules/socket.io/client-dist"));
@@ -27,45 +25,10 @@ app.use(function (req, res, next) {
 
 const port = process.env.PORT || 2411;
 
-app.listen(port, () => console.log(`Node app listening on port ${port}!`));
+http.listen(port, () => console.log(`Node app listening on port ${port}!`));
 
 const {contactList,messagesList}=require("./data")
 
-// app.get("/contacts",function(req,res){
-// res.send(contactList);
-// })
-
-
-
-// // app.post("/message",function(req,res){
-// //     let body=req.body;
-// //     messagesList.push(body);
-// //     res.send(body);
-// // })
-
-// app.get("/message",function(req,res){
-//     res.send(messagesList);
-// })
-
-
-// io.on("connection", function (socket) {
-//     console.log("A user connected");
-  
-//     // Handle new messages
-//     socket.on("newMessage", function (message) {
-//       messagesList.push(message);
-//       io.emit("messageReceived", message);
-//     });
-  
-//     // Handle disconnection
-//     socket.on("disconnect", function () {
-//       console.log("A user disconnected");
-//     });
-//   });
-
-// Additional server configurations and routes...
-
-// Load data
 app.use(express.static("public"));
 
 // Routes
@@ -84,11 +47,23 @@ app.get("/messages", function (req, res) {
   res.send(messagesList);
 });
 
-io.on("connection", function (socket) {
-    console.log("A user connected");
+// io.on("connection", function (socket) {
+//     console.log("A user connected");
   
-    socket.on("disconnect", function () {
-      console.log("User disconnected");
+//     socket.on("disconnect", function () {
+//       console.log("User disconnected");
+//     });
+//   });
+  
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("message", (data) => {
+      console.log("Received message:", data);
+      io.emit("newMessage", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("A user disconnected");
     });
   });
-  
